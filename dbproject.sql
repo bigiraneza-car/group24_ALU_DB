aefbc8eb3cc3d7905fbe480496c7be713d759a26
rsodlulash <r.sodlulash@alustudent.com>
2026-07-17 06:21:23 -0700

    Faculty table

---
+++ dbproject.sql
@@ -0,0 +1,20 @@
+-- Creation of Database
+CREATE DATABASE alu_db;
+
+-- Creation of faculty table
+USE alu_db;
+CREATE TABLE Faculty(
+    faculty_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    department VARCHAR(50)
+);
+
+INSERT INTO Faculty(faculty_id, name, email, department)
+VALUES
+    (20241, 'Zintle',  'zintle@sustudent.com',  'BEng'),
+    (20242, 'Amazing', 'amazing@sustudent.com', 'BEd'),
+    (20243, 'Buhle',   'buhle@sustudent.com',   'BAgric'),
+    (20245, 'Sibu',    'subu@sustudent.com',    'AppMath');
+
+SELECT * FROM Faculty;
